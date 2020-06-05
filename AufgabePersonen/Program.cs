05ca9973dce4ab7f7275dcff565d13f2e43e3b9c
manger0 <mangerer1994@gmail.com>
2020-06-05 10:27:56 +0200

    first commit

---
+++ AufgabePersonen/Program.cs
@@ -0,0 +1,50 @@
+﻿using AufgabePersonen.models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AufgabePersonen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myFileContent = File.ReadAllLines("C:\\Users\\DCV\\Desktop\\C#\\AufgabePersonen\\persons.txt");
+
+            List<Person> Persons = new List<Person>();
+            foreach (var i in myFileContent)
+            {
+
+                Console.WriteLine(i);
+                string[] splittedValues = i.Split(";");
+
+                switch (splittedValues[0])
+                {
+                    case "Hans":
+                        Person person1 = new Person(splittedValues[0], Convert.ToInt32(splittedValues[1]), splittedValues[2], "Coder" );
+                        Persons.Add(person1);
+                        break;
+                    case "Helga":
+                        Person person2 = new Person(splittedValues[0], Convert.ToInt32(splittedValues[1]), splittedValues[2], "Coder");
+                        Persons.Add(person2);
+                        break;
+                    case "Fritz":
+                        Person person3 = new Person(splittedValues[0], Convert.ToInt32(splittedValues[1]), splittedValues[2], "Officer");
+                        Persons.Add(person3);
+                        break;
+                    case "Frida":
+                        Person person4 = new Person(splittedValues[0], Convert.ToInt32(splittedValues[1]), splittedValues[2], "Officer");
+                        Persons.Add(person4);
+                        break;
+                }
+            }
+            List<string> output = new List<string>();
+            foreach (Person person in Persons)
+            {
+                output.Add(person.Name + ";" + person.Age + ";" + person.City + ";" + person.Job);
+                Console.WriteLine(person.Name + ";" + person.Age + ";" + person.City + ";" + person.Job);
+            }
+            File.WriteAllLines("C:\\Users\\DCV\\Desktop\\C#\\AufgabePersonen\\personseddited.txt", output);
+        }
+    }
+}
---
+++ AufgabePersonen/models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AufgabePersonen.models
+{
+    public class Person
+    {
+        public string Name, City, Job;
+        public int Age;
+        public Person(string Name, int Age, string City, string Job)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.City = City;
+            this.Job = Job;
+        }
+
+
+
+
+    }
+}
